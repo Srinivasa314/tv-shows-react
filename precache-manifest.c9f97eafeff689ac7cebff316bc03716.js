26fe691c8fa362d3763672089f916072d42f1e27
Srinivasa <m.srinivasa1234@gmail.com>
2020-06-14 10:41:07 +0530

    Added build folder

---
+++ precache-manifest.c9f97eafeff689ac7cebff316bc03716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9ad9825d58d81f1ab80cd55b84419a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82362803a8d70091c801",
+    "url": "/static/css/main.7562275d.chunk.css"
+  },
+  {
+    "revision": "35ca3f1329a32ce7e3f9",
+    "url": "/static/js/2.75b6e8b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.75b6e8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82362803a8d70091c801",
+    "url": "/static/js/main.57ba8dbc.chunk.js"
+  },
+  {
+    "revision": "adad79239d658f261bb8",
+    "url": "/static/js/runtime-main.766ee72e.js"
+  }
+]);
