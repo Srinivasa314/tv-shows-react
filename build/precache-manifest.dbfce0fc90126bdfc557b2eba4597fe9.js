d8cb2d526c0d832a2ed9710bbdd7baa8bd262eb5
Srinivasa <m.srinivasa1234@gmail.com>
2020-06-14 10:52:22 +0530

    Changed homepage

---
+++ build/precache-manifest.dbfce0fc90126bdfc557b2eba4597fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13487efcb0db70b2db31b3a24316fea2",
+    "url": "/tv-shows-react/index.html"
+  },
+  {
+    "revision": "82362803a8d70091c801",
+    "url": "/tv-shows-react/static/css/main.7562275d.chunk.css"
+  },
+  {
+    "revision": "35ca3f1329a32ce7e3f9",
+    "url": "/tv-shows-react/static/js/2.75b6e8b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tv-shows-react/static/js/2.75b6e8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82362803a8d70091c801",
+    "url": "/tv-shows-react/static/js/main.57ba8dbc.chunk.js"
+  },
+  {
+    "revision": "b1d4c78c8f2362a9f71f",
+    "url": "/tv-shows-react/static/js/runtime-main.39d3ed89.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9f97eafeff689ac7cebff316bc03716.js"
+  "/tv-shows-react/precache-manifest.dbfce0fc90126bdfc557b2eba4597fe9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tv-shows-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
